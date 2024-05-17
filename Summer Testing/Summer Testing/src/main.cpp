9fd40d3d5190d235f58bf5867424f4d5d810928f
Lucan <chuganilucan@gmail.com>
2024-05-17 09:25:42 -0700

    Setup new LemLib release for Summer testing
    1. Created project for Summer Testing
    2. Added LemLib release 0.5.0 using pros 3.8.3
    3. Setup sensors, chassis, untuned PID's
    4. Added split arcade driver control with dead zone

---
+++ Summer Testing/Summer Testing/src/main.cpp
@@ -0,0 +1,132 @@
+#include "main.h"
+#include "lemlib/api.hpp"
+
+/* ------------------------------- Controller ------------------------------- */
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+/* ---------------------------- Drivetrain Motors --------------------------- */
+pros::Motor left_front(-2, pros::E_MOTOR_GEAR_BLUE); // port 1, reversed, blue cart
+pros::Motor left_back(-12, pros::E_MOTOR_GEAR_BLUE); // port 2, reversed, blue cart
+
+pros::Motor right_front(9, pros::E_MOTOR_GEAR_BLUE); // port 3, forwards, blue cart
+pros::Motor right_back(19, pros::E_MOTOR_GEAR_BLUE); // port 4, forwards, blue cart
+
+/* ------------------------------ Motor Groups ------------------------------ */
+pros::MotorGroup left_drive({left_front, left_back});
+pros::MotorGroup right_drive({right_front, right_back});
+
+/* --------------------------------- Sensors -------------------------------- */
+pros::Imu inertial(14);
+
+pros::Rotation vert_rotation(18, true); // port 18, reversed
+pros::Rotation hor_rotation(20, false); // port 20, forwards
+
+/* -------------------------- Tracking Wheel Setup -------------------------- */
+lemlib::TrackingWheel vert_tracking_wheel(&vert_rotation, lemlib::Omniwheel::NEW_275, -1);
+lemlib::TrackingWheel hor_tracking_wheel(&hor_rotation, lemlib::Omniwheel::NEW_275, -2);
+
+
+/* --------------------------- Drivetrain Setup -------------------------- */
+lemlib::Drivetrain drivetrain(
+	&left_drive, 				// left motor group
+    &right_drive, 				// right motor group
+    10, 						// 10" track width
+    lemlib::Omniwheel::NEW_275,	// using new 2.75" omnis
+	450, 						// drivetrain rpm is 450
+    2 							// horizontal drift is 2
+);
+
+lemlib::OdomSensors sensors(
+	&vert_tracking_wheel, 		// Vert tracking 1
+    nullptr, 					// Vert trackin 2
+    &hor_tracking_wheel, 		// Hor tracking 1
+    nullptr, 					// Hor tracking 2
+    &inertial 					// Inertial
+);
+
+/* -------------------------------- PID Setup ------------------------------- */
+lemlib::ControllerSettings lateral_controller(
+	10,		// proportional gain (kP)
+    0,		// integral gain (kI)
+    3,		// derivative gain (kD)
+    3,		// anti windup
+    1,		// small error range, in inches
+    100,	// small error range timeout, in milliseconds
+    3, 		// large error range, in inches
+	500, 	// large error range timeout, in milliseconds
+    20 		// maximum acceleration (slew)
+);
+
+lemlib::ControllerSettings angular_controller(
+	2, 		// proportional gain (kP)
+    0, 		// integral gain (kI)
+    10, 	// derivative gain (kD)
+    3, 		// anti windup
+    1, 		// small error range, in degrees
+    100, 	// small error range timeout, in milliseconds
+    3, 		// large error range, in degrees
+    500, 	// large error range timeout, in milliseconds
+    0 		// maximum acceleration (slew)
+);
+
+/* ----------------------------- Create Chassis ----------------------------- */
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+                        lateral_controller, // lateral PID settings
+                        angular_controller, // angular PID settings
+                        sensors // odometry sensors
+);
+
+
+/* -------------------------------- Variables ------------------------------- */
+int dead_zone = 5;
+
+/* -------------------------------------------------------------------------- */
+/*                              Control Functions                             */
+/* -------------------------------------------------------------------------- */
+
+void initialize() {
+	pros::lcd::initialize();
+    chassis.calibrate();
+	
+    pros::Task screen_task([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); 			// X position
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); 			// Y position
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta);	// Heading degrees
+
+            // delay to save resources
+            pros::delay(20);
+        }
+    });
+}
+
+void disabled() {}
+
+void competition_initialize() {}
+
+void autonomous() {}
+
+void opcontrol() {
+	while (true) {
+		/* --------------------------- Drivetrain Control --------------------------- */
+        // Get joystick positions
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+
+        // Deadzone
+		if (abs(leftY) < dead_zone) {
+            leftY = 0;
+        }
+        if (abs(rightX) < dead_zone) {
+            rightX = 0;
+        }
+
+        // Move motors
+        left_drive.move(leftY + rightX);
+        right_drive.move(leftY - rightX);
+
+		// Delay to save resources
+		pros::delay(20);
+	}
+}
